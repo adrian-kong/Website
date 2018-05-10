5c1f9e580841b9ec33218e0e4bf2dd7bfc2caf92
adrian-kong <35755741+adrian-kong@users.noreply.github.com>
2018-05-11 09:44:54 +1000

    Update main.js

--- main.js
+++ main.js
@@ -10,13 +10,6 @@
       array.splice(array.indexOf(selected), 1);
     }
 
-$(window).scroll(function () {
+$(window).on("scroll touchmove", function () {
     $('#header_nav').toggleClass('tiny', $(document).scrollTop() > 0);
-    $('.fade').each( function(i){
-        var bottom_of_object = $(this).position().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        if( bottom_of_window > bottom_of_object ){        
-            $(this).animate({'opacity':'1'},1500);
-        }
-    });
 });
